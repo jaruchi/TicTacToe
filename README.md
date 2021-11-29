8017007040a417fe43de22a49919ceb5abbd5673
Ruchi Agarwal <ruchismailbox@gmail.com>
2021-11-29 13:33:40 -0600

    commiting MVP

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # TicTacToe
 
+### Technologies Used
 Tic Tac Toe game using HTML, CSS and JavaScript.
+
+### Description
+Tic-tac-toe, or Xs and Os is a paper-and-pencil game for two players who take turns marking the spaces in a three-by-three grid with X or O. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner.
 
 ### Step I: Initial Game Board design
 There is a heading on the page with a 3X3 game board.
@@ -10,3 +14,79 @@ The game board consists of a game container which has 3 game rows. Every game ro
 ### Step II: Game Design with possible elements
 Added semantics like header, main, footer in HTML. Manipulated CSS for different layouts using flex property.
 ![Game Page Layout](/docs/layout.png)
+
+### Bronze MVP
+ An HTML page with a layout of 9 cells and different game components like setting of players and their scores, game status like winning, loose or draw, reset & restart buttons.
+ CSS to make the page layout visible.
+ Dummy JS functions to test functonality of setting up the game and accessing the elements by filling them with 'X' on each click.
+
+ ### Pseudocode
+ Make the game board using HTML and CSS
+ Initialize all the event handlers for the cell clicks
+ On first cell click update its value with X as its the statring turn:
+    Update the game board
+    Set the game status
+    Switch turns between X and O
+
+  ### Pseudocode For UpdateGameBoard
+  updateGameBoard:
+    Iterate the gameBoard array according to elementID:
+        Insert the X or O on the specified location
+
+### Pseudocode For setGameStatus
+  setGameStatus:
+    for every click check:
+        if currentPlayer Won
+            then stopGame
+                if currentTurn==='X'
+                    updateGameStatus for X
+                    calculateScore for X
+                else
+                    updateGameStatus for O
+                    calculateScore for O
+        ElseIf check for isAllCellFilled
+                then stopGame
+                updateGameStatus for Draw 
+        Else
+            check for CurrentTurn and updatestatus for next player turn
+
+### Pseudocode For isAllCellFilled
+  find for any empty cell from gameboard
+  if yes return false
+  else return true
+
+  ### Pseudocode For currentPlayerWon
+  Iterate the const array defined for 8 winning conditions
+    for every index 
+        get the values for those indexes in the gameBoard
+        check if all are Equal
+        if yes return true
+        else return false
+
+
+### Pseudocode For restartGame
+  make gameBoard empty or to its initial stage
+  set gameElements to empty
+  empty gameStatus
+  set currentTurn to X
+
+
+### User Stories Covered
+As a user, I should be able to start a new tic tac toe game
+As a user, I should be able to click on a square to add X first and then O, and so on
+As a user, I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
+As a user, I should not be able to click the same square twice
+As a user, I should be shown a message when I win, lose or tie
+As a user, I should not be able to continue playing once I win, lose, or tie
+As a user, I should be able to play the game again without refreshing the page
+
+  
+
+
+
+
+ 
+
+
+
+
--- script.js
+++ script.js
@@ -1,18 +1,223 @@
-function boardIntialization() {
-    const gameCells = document.querySelectorAll(".game-cell");
-    gameCells.forEach(gameCell => gameCell.addEventListener("click", onGameCellClick))
-}
 
-function onGameCellClick(e) {
-    let elem = e.target;
-    if (e.target.classList.contains('game-cell')) {
-         elem = e.target.querySelector('.game-element');
+//Winning Conditions Array
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+//Game Status Object for different Status
+const gameStatus = {
+    1: 'X Turn',
+    2: 'O Turn',
+    3: 'X Won',
+    4: 'O Won',
+    5: 'Game Draw'
+};
+  
+class GameBoard {
+    constructor() {
+       
+        // to capture all the cells data
+        this.gameBoard =  [
+            ['c00', 'c01', 'c02'],
+            ['c10', 'c11', 'c12'],
+            ['c20', 'c21', 'c22']
+        ]; 
+
+        this.currentTurn = 'X'; // game always starts from X turn
+
+        // Setting Starting score for both players as 0
+        this.playersScore = {
+            playerXScore: 0,
+            playerOScore: 0
+        };
+
+        // token to check when to stop the game after a certain condition is met
+        this.gameIsStopped = false;
+        this.initCells(); // to get different elements from the HTML
+
     }
-    elem.innerText = 'X';
+    
+    initCells() {
+        this.gameCells = document.querySelectorAll(".game-cell");
+        this.gameElements = document.querySelectorAll(".game-element");
+        this.gameStatusElement = document.querySelector("#game-status");
+
+        this.scoreXElement = document.querySelector("#scoreX");
+        this.scoreXElement.innerText = this.playersScore.playerXScore;
+        this.scoreOElement = document.querySelector("#scoreO");
+        this.scoreOElement.innerText = this.playersScore.playerOScore;
+
+
+
+        //this.gameCells.forEach(gameCell => gameCell.addEventListener("click",this.onGameCellClick));/// it passes the callback with particular cell data
+        this.gameCells.forEach(gameCell => gameCell.addEventListener("click", e => this.onGameCellClick(e)));/// (event)callback function don't support functionality of this keyword
+
+        this.restartButton = document.querySelector("#restart");
+        this.restartButton.addEventListener("click", () => this.onRestartClick());
+
+        this.resetButton = document.querySelector("#reset");
+        this.resetButton.addEventListener("click", () => this.onResetClick());
+
+    }
+// Function to perform on every cell click
+    
+    onGameCellClick(e) {
+        if (this.gameIsStopped) {
+            return;
+        }
+        //console.log(e.currentTarget,this)
+        let elem = e.currentTarget;
+        let gameElement = elem.firstElementChild;
+        
+        let elementId = elem.id;
+        if (gameElement.innerText === '') {
+            //elem.innerText = this.currentTurn;
+            gameElement.innerText = this.currentTurn;
+            this.updateGameBoard(elementId);
+            this.setGameStatus();
+            this.switchTurn();
+        }
+    }
+
+// Functionality for changing players between X & O 
+    
+    switchTurn() {
+        console.log(this.currentTurn);
+        if (this.currentTurn === 'X') {
+            this.currentTurn = 'O';
+        }
+        else {
+            this.currentTurn = 'X';
+        }
+    }
+
+    updateGameBoard(elementId) { 
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (this.gameBoard[i][j] === elementId) {
+                    this.gameBoard[i][j] = this.currentTurn;
+                }
+            }
+        } 
+    }
+
+    setGameStatus() {
+        if (this.isCurrentPlayerWon()) {// Player Won
+            this.stopGame();
+            if (this.currentTurn === 'X') {
+                this.updateGameStatus(3);
+                this.calculateXScore()
+            }
+            else {
+                this.updateGameStatus(4);
+                this.calculateOScore();
+            }
+        } else if (this.isAllCellFilled()) {//Draw
+            this.stopGame();
+            this.updateGameStatus(5);
+        }
+        else {
+            if (this.currentTurn === 'X') {// next turn
+                this.updateGameStatus(2);
+            }
+            else {
+                this.updateGameStatus(1);
+            }
+        }
+
+    }
+
+    calculateXScore() {
+        this.playersScore.playerXScore++;
+        this.scoreXElement.innerText =  this.playersScore.playerXScore;
+    }
+
+    calculateOScore() {
+        this.playersScore.playerOScore++;
+        this.scoreOElement.innerText =  this.playersScore.playerOScore;
+    }
+
+    isCurrentPlayerWon() {
+        for (let index = 0; index < winningConditions.length; index++) {
+            const winCon = winningConditions[index];
+            const result = this.isAllCellsFilledWithSameValue(winCon);
+            if (result) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    isAllCellsFilledWithSameValue(winCon) {
+        const gameBoardFlat = this.gameBoard.flat();
+        const a = gameBoardFlat[winCon[0]];
+        const b = gameBoardFlat[winCon[1]];
+        const c = gameBoardFlat[winCon[2]];
+        if (a === b && a === c) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    updateGameStatus(statusState) {
+        //const gameStatusElement = document.querySelector("#game-status");
+        this.gameStatusElement.innerText = gameStatus[statusState];
+    }
+
+    isAllCellFilled() {
+        const gameBoardFlat = this.gameBoard.flat();
+        if (gameBoardFlat.find(data => data !== 'X' && data !== 'O')) {
+            return false;
+        }
+        return true;
+    }
+
+    stopGame() {
+        this.gameIsStopped = true;
+        //console.log("game stopped");
+    }
+
+    onResetClick(e) {
+        this.onRestartClick();
+        // score
+        this.playersScore = {
+            playerXScore: 0,
+            playerOScore: 0
+        };
+        this.scoreXElement.innerText = this.playersScore.playerXScore;
+        this.scoreOElement.innerText =  this.playersScore.playerOScore;
+
+    }
+
+    onRestartClick(e) {
+        //this.gameboard empty
+        this.gameBoard = [
+            ['c00', 'c01', 'c02'],
+            ['c10', 'c11', 'c12'],
+            ['c20', 'c21', 'c22']
+        ];
+        //game elements reset
+        this.gameElements.forEach(elm=>elm.innerText='')
+        //game status empty
+        this.gameStatusElement.innerText = '';
+        //current turn = x
+        this.currentTurn = 'X';
+        //gamestopped = false
+        this.gameIsStopped = false;        
+    }
 }
 
 function init() {
-    boardIntialization();
+    new GameBoard();
 }
 
 window.addEventListener('load', init);
